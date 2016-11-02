e574469c48931d2ca700f56cd96516b2176af309
Joel McCoy <joelmccoy+github@gmail.com>
2016-11-01 21:23:00 -0400

    First commit, first bot

---
+++ reuters_news.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+const _ = require('lodash');
+const mime = require('mime');
+
+const path = require('path');
+const P = require('bluebird');
+const jsdom = P.promisifyAll(require('jsdom'));
+const request = P.promisifyAll(require('request'));
+const fs = P.promisifyAll(require('fs'));
+
+const creds = require('./credentials');
+const Twit = require('twit');
+const REST = new Twit(creds.live);
+REST.postMediaChunkedAsync = P.promisify(REST.postMediaChunked);
+
+const REUTERS_IMAGES_URI = 'https://pictures.reuters.com';
+
+jsdom.envAsync(REUTERS_IMAGES_URI).
+then(w => {
+  let imgs = w.document.querySelectorAll('img');
+  let srcs = _.map(imgs, img=>img.src);
+  // remove AJAX circle and nav arrows:
+  srcs = srcs.filter(src => !~src.indexOf('RTR/Images') && !~src.indexOf('TRMisc'));
+  let uris = _.sampleSize(srcs, 4);
+  return uris;
+}).
+map(uri => {
+  let basename = path.basename(uri);
+  console.log(`basename of ${basename}`);
+  return request.getAsync({uri: uri, encoding: null})
+    // then(data => {console.log(`got ${data.length} long data`); fs.writeFileAsync(basename, data, {encoding: null})}).
+}).
+map(res => {
+  let media_data = res.body.toString('base64');
+  return REST.post('media/upload',  {media_data: media_data});
+}).
+map(reply => reply.data.media_id_string).
+then(media_ids => {
+  console.log(`media_ids: ${media_ids}`);
+  let status = "we live in a world of contrasts";
+  let params = {status: status, media_ids: media_ids};
+  return REST.post('statuses/update', params);
+})
+.then(res => {
+  console.log(`I twote:\n${JSON.stringify(_.pick(res.data,['id_str', 'text', 'entities']), null, 2)}`);
+}).
+catch(console.error);
