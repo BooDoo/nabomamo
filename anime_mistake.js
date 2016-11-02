8402db0168b89f2ab653953592bc773bace2479c
Joel McCoy <joelmccoy+github@gmail.com>
2016-11-02 00:54:32 -0400

    imagemagick-native is too finicky; use gm

--- anime_mistake.js
+++ anime_mistake.js
@@ -5,7 +5,7 @@ const P = require('bluebird');
 const path = require('path');
 const fs = P.promisifyAll(require('fs'));
 
-const imagemagick = P.promisifyAll(require('imagemagick-native'));
+const gm = require('gm').subClass({imageMagick: true});
 const creds = require('./credentials');
 const Twit = require('twit');
 const REST = new Twit(creds.live);
@@ -15,19 +15,12 @@ const ANIME_BASENAME = _.sample(fs.readdirSync(ANIME_DIR));
 const ANIME_PATH = path.join(ANIME_DIR, ANIME_BASENAME);
 const TEMPLATE_PATH = path.join('assets', 'base', 'miyazaki-mistake_720.png');
 
-P.all([fs.readFileAsync(ANIME_PATH), fs.readFileAsync(TEMPLATE_PATH)]).
-then(data => {
-  let animeData = data[0],
-      templateData = data[1],
-      animeOptions = {srcData: animeData, height: 720, width: 500, resizeStyle: 'aspectfit'};
-  if (!!_.random()) {
-    animeOptions.flip = true;
-    animeOptions.rotate = 180;
-  }
+let compBuffer = gm(TEMPLATE_PATH).composite(ANIME_PATH).resize(1280, 720).gravity('NorthEast')
+//give me a promise!
+compBuffer.__proto__.toBufferAsync = P.promisify(compBuffer.__proto__.toBuffer);
 
-  animeData = imagemagick.convert(animeOptions);
-  let compData = imagemagick.composite({srcData: templateData, compositeData: animeData, gravity: 'NorthEastGravity'});
-  let media_data = compData.toString('base64');
+compBuffer.toBufferAsync().then(buffer => {
+  let media_data = buffer.toString('base64');
   return REST.post('media/upload',  {media_data: media_data});
 }).
 then(reply => reply.data.media_id_string).
