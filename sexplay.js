8fe6479633a4fc5f8fe83ea4d09b6dfb2e21ecac
Joel McCoy <joelmccoy+github@gmail.com>
2016-11-04 18:06:11 -0400

    SEXPLAY, baby

---
+++ sexplay.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+const _ = require('lodash');
+
+const creds = require('./credentials');
+const Twit = require('twit');
+const REST = new Twit(creds.live);
+
+const CORPORAPATH = './assets/corpora-project/';
+
+const objects = require(CORPORAPATH+'objects/objects').objects;
+const verbs = require(CORPORAPATH+'words/verbs').verbs.map(verb=>verb.present);
+const building_materials = require(CORPORAPATH+'materials/building-materials')["building materials"];
+const layperson_metals = require(CORPORAPATH+'materials/layperson-metals')["layperson metals"];
+const greek_gods = require(CORPORAPATH+'mythology/greek_gods').greek_gods;
+const monsters = require(CORPORAPATH+'mythology/monsters').names;
+const norse_deities = _(require(CORPORAPATH+'mythology/norse_gods').norse_deities).valuesIn().flatten().value();
+const bodyparts = require(CORPORAPATH+'humans/bodyParts').bodyParts;
+
+
+const words = _.concat(objects, verbs, building_materials, layperson_metals, greek_gods, monsters, norse_deities, bodyparts).
+  map(item=>item.replace(/[ \.,:;\!\?'"]/g,''));
+
+const quals = ["is", "is not", "isn't", "is never", "is always", 
+    "will never be", "has always been", "might be", "could be",
+    "must be", "will soon be", "doesn't sounds like", "sounds a lot like",
+    "can't be", "is now", "has become", "is my kind of"];
+
+let status = `"${_.sample(words)}play" ${_.sample(quals)} "a sex thing"`;
+
+REST.post('statuses/update', {status: status}).
+then(res => {
+  console.log(`SEXPLAY twote:\n${res.data.id_str}, ${res.data.text}`);
+}).
+catch(console.error)
